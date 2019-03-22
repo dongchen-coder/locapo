076f75e2dfdcda86d6a83a6e5315caab1648c3e4
Dong Chen <dchen@Alita.local>
2019-03-21 22:27:14 -0400

    add lru 64B CLS reuslt & MRC & update plots

--- plot.py
+++ plot.py
@@ -1,16 +1,25 @@
 import os
 import matplotlib.pyplot as plt
 
-path = "./poly_trace_lru/"
-files = os.listdir(path)
+lru_path = "./poly_trace_lru/"
+files = os.listdir(lru_path)
+
+lru_mrc_path = "./poly_trace_lru_mrc/"
+
+rll_path = "./poly_trae_rll/"
 
 figPath = "./plots/"
 
 for f in files:
-	content = open(path + f, "r")
+	
+	#if ('mvt' not in f):
+	#	continue
+
+	content = open(lru_path + f, "r")
 
 	rdHist = {}
 	totalCnt = 0
+	coldMiss = 0
 
 	for line in content:
 		if ("BIN:" in line and "VAL:" in line and "COUNT:" in line):
@@ -21,6 +30,9 @@ for f in files:
 				continue
 			rdHist[rd] = cnt
 			totalCnt += cnt
+		elif ("Total number of data blocks is" in line):
+			lineList = line.split()
+			coldMiss = int(lineList[7])
 
 	content.close()
 	#print rdHist
@@ -31,11 +43,18 @@ for f in files:
 
 	cacheSizes.append(0)
 	missRatios.append(1)
-	misses = totalCnt
+	misses = totalCnt + coldMiss
+	
+	content = open(lru_mrc_path + f.replace("_trace_result", "_miss_ratio_curve"),"w")
+	content.write("0 1\n")
 	for key in sorted(rdHist.keys()):
-		cacheSizes.append(key * 64 / 1024)
+		cacheSizes.append(float((key + 1) * 64) / 1024)
 		misses -= rdHist[key]
 		missRatios.append(float(misses)/totalCnt)
+		content.write(str(float((key + 1) * 64) / 1024) + " " + str(float(misses)/totalCnt) + "\n")
+	content.close()
+	#print cacheSizes
+	#print missRatios
 
 	plt.plot(cacheSizes, missRatios)
 	plt.title(f.split('_')[0])
